e4673370b073c9d6a3984ce62fbbf24d85a439f5
AriosiLucas <ariosilucas@gmail.com>
2024-11-23 15:02:46 -0300

    Adiciona rotas e controllers de usuário e de login

--- src/routes/api.php
+++ src/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\AmbienteController;
+use App\Http\Controllers\UserController;
 
 // ...existing code...
 
@@ -13,5 +14,11 @@ Route::middleware(['cors'])->group(function () {
     Route::put('/ambientes/{id}', [AmbienteController::class, 'update']); // Atualizar um ambiente específico
     Route::delete('/ambientes/{id}', [AmbienteController::class, 'destroy']); // Deletar um ambiente específico
 });
+
+Route::get('/users', [UserController::class, 'index'])->name('getUsers');
+Route::get('/users/{id}', [UserController::class, 'show'])->name('getUser');
+Route::post('/users', [UserController::class, 'store'])->name('createUser');
+Route::put('/users/{id}', [UserController::class, 'update'])->name('updateUser');
+Route::delete('/users/{id}', [UserController::class, 'destroy'])->name('deleteUser');
 
 Route::post('/login', [AuthController::class, 'login']);
